c90033ebed7a7eaccff58b843ed67c843cb67450
jnrsai24 <jnrsai24@gmail.com>
2023-06-28 12:29:38 +0000

    all test passed still css is pending

--- src/App.js
+++ src/App.js
@@ -1,3 +1,20 @@
-const App = () => <h1>Remove this heading and write your code here</h1>
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import Home from './components/Home'
+import Header from './components/Header'
+import About from './components/About'
+import Contact from './components/Contact'
+import NotFound from './components/NotFound'
+
+const App = () => (
+  <BrowserRouter>
+    <Header />
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/contact" component={Contact} />
+      <Route component={NotFound} />
+    </Switch>
+  </BrowserRouter>
+)
 
 export default App
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,1 +1,33 @@
 // Write your JS code here
+import {Link} from 'react-router-dom'
+// import Home from '../Home'
+// import About from '../About'
+// import Contact from '../Contact'
+import './index.css'
+
+const Header = () => (
+  <div>
+    <nav>
+      <div>
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/wave-logo-img.png"
+          alt="wave"
+        />
+        <h1>Wave</h1>
+      </div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+    </nav>
+  </div>
+)
+
+export default Header
